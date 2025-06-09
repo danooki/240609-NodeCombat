49b66a2637f1292539ca661af42aabd224641127
Daniel Fuentealba <danielfuentealba86@gmail.com>
2025-06-09 22:56:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Allowed Pokémon types (6 basic ones)
+const validTypes = [
+  "Fire",
+  "Water",
+  "Grass",
+  "Lightning",
+  "Psychic",
+  "Fighting",
+];
+
+// grab the argument from the command line
+const args = process.argv.slice(2);
+const chosenType = args[0];
+
+// If no type is provided
+if (!chosenType) {
+  console.log("Please choose a Pokémon type.");
+  console.log("Available types: " + validTypes.join(", "));
+  process.exit(1);
+}
+
+// Success!
+console.log(`✅ You chose: ${chosenType} type Pokémon!`);
