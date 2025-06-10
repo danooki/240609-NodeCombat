fee772d4eb26c4792177c35666104fd66c398484
Daniel Fuentealba <danielfuentealba86@gmail.com>
2025-06-10 14:30:32 +0200

    steps defined

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-// Allowed Pokémon types (6 basic ones)
-const validTypes = [
-  "Fire",
-  "Water",
-  "Grass",
-  "Lightning",
-  "Psychic",
-  "Fighting",
-];
+// Valid Pokémon types
+const validTypes = ["Fire", "Water", "Grass", "Poison", "Psychic", "Metal"];
 
 // grab the argument from the command line
 const args = process.argv.slice(2);
 const chosenType = args[0];
 
-// If no type is provided
+// if no type is provided
 if (!chosenType) {
-  console.log("Please choose a Pokémon type.");
+  console.log("\x1b[32m%s\x1b[0m", "Please choose a Pokémon type.");
   console.log("Available types: " + validTypes.join(", "));
   process.exit(1);
 }
 
-// Success!
-console.log(`✅ You chose: ${chosenType} type Pokémon!`);
+// chosen type
+console.log(`You chose: ${chosenType} type Pokémon!`);
+
+// have to write the type and console has to acknowledge it.
+
+// computer choses a random one too
+
+// battle happens
+
+// many if then.
+
+// obtain result of the fight.
